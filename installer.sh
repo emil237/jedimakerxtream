6b4494a1ee51125a57a650a73b418ccb4d69625e
emil237 <marianatalaat1@gmail.com>
2023-01-17 10:24:32 -0800

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,45 +1,44 @@
 #!/bin/sh
-# ###########################################
-# SCRIPT : DOWNLOAD AND INSTALL PLUGIN JEDIMAKERXTREAM  #
-# ###########################################
-##setup command=wget https://raw.githubusercontent.com/emil237/jedimakerxtream/main/installer.sh -qO - | /bin/sh
 #
-# ###########################################
-echo "***********************************************************************************************************************"
-# Config script #
-TMPDIR='/tmp'
-VERSION='6.21'
-MY_URL='https://raw.githubusercontent.com/emil237/jedimakerxtream/main'
+# SCRIPT : DOWNLOAD AND INSTALL PLUGIN JEDIMAKERXTREAM  #
+#wget -q "--no-check-certificate" https://raw.githubusercontent.com/emil237/jedimakerxtream/main/installer.sh -O - | /bin/sh
+
+##########################################
+VERSION="6.21"
+#############################################################
+TEMPATH=/tmp
+OPKGINSTALL="opkg install --force-overwrite"
 MY_IPK="enigma2-plugin-extensions-jedimakerxtream_6.21_all.ipk"
 MY_DEB="enigma2-plugin-extensions-jedimakerxtream_6.21_all.deb"
-####################
-MY_EM='============================================================================================================'
+MY_URL="https://raw.githubusercontent.com/emil237/jedimakerxtream/main"
+if [ -f /etc/apt/apt.conf ] ; then
+    STATUS='/var/lib/dpkg/status'
+    OS='DreamOS'
+elif [ -f /etc/opkg/opkg.conf ] ; then
+   STATUS='/var/lib/opkg/status'
+   OS='Opensource'
+fi
+
 #  Remove Old Plugin  #
 
 echo "   >>>>   Remove old version   "
 
 rm -r /usr/lib/enigma2/python/Plugins/Extensions/JediMakerXtream
 
-#################################
-    
-###################
-echo "============================================================================================================================"
- echo " DOWNLOAD AND INSTALL PLUGIN "
-
-echo "   Install Plugin please wait "
-cd /tmp 
+echo ""
+# Download and install plugin
+cd /tmp
 set -e
-if which dpkg > /dev/null 2>&1;
-wait
-wget "$MY_URL/$MY_DEB"
+ if which dpkg > /dev/null 2>&1; then
+  wget "$MY_URL/$MY_DEB"
 		dpkg -i --force-overwrite $MY_DEB; apt-get install -f -y
 wait
-rm -f /tmp/$MY_DEB
+rm -f $MY_DEB
 	else
-wget "$MY_URL/$MY_IPK"
-		opkg install --force-overwrite $MY_IPK
+  wget "$MY_URL/$MY_IPK"
+		$OPKGINSTALL $MY_IPK
 wait
-rm -f /tmp/$MY_IPK
+rm -f $MY_IPK
 	fi
 echo "================================="
 set +e
@@ -49,44 +48,15 @@ wait
 echo ">>>>  SUCCESSFULLY INSTALLED <<<<"
 fi
 		echo "********************************************************************************"
-echo "   UPLOADED BY  >>>>   EMIL_NABIL " 
+echo "   UPLOADED BY  >>>>   EMIL_NABIL "   
 sleep 4;
-echo "  Develop by : KiddaC "                            
-echo "$MY_EM"
-###################                                                                                                                  
-echo " your Device will RESTART Now " 
-echo "**********************************************************************************"
-wait
-init 4
-init 3
+		echo "#                Restart Enigma2 GUI                    #"
+echo "#########################################################"
+sleep 2
+if [ $OS = 'DreamOS' ]; then
+    systemctl restart enigma2
+else
+    killall -9 enigma2
+fi
 exit 0
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
